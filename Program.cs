898513a18702aa7308a555e5866f5889f1d96515
FCKTHISCODE <108792193+FCKTHISCODE@users.noreply.github.com>
2022-09-18 14:55:44 +0300

    Task has been completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5] { "Hello", "HI", "Bye", "Wazzup", "Ola" }; // Array 
+string[] SortedArray = new string[array.Length]; //Sorted array init
+
+void SortArray (string[] array, string[] SortedArray) //Sort array function
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++) //find ellement with lenght less or equal 3
+    {
+        if (array[i].Length <= 3) 
+        {
+            SortedArray[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array) //Print array
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+SortArray(array, SortedArray);
+PrintArray(SortedArray);
